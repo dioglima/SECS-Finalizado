438d58c6b8324583de093362324e4f8c7e67d0e2
Isaac Daniel <isaacbusiness2019@hotmail.com>
2024-04-14 22:37:59 -0300

    chamando getUserData nos modais ao confirmar

--- js/dashboard.js
+++ js/dashboard.js
@@ -36,42 +36,7 @@ async function getUserData() {
         return response.json();
     })
     .then(function (data) {
-        const transactions = [
-            {
-              "id": "d721f801-7f62-498e-a7d0-700b44604572",
-              "value": 2000,
-              "description": "Sobre alguma coisa",
-              "type": "INCOME",
-              "userId": "6a04f4a5-4df3-4b1b-b4ea-02efbe5ea9ce",
-              "categoryId": null,
-              "createdAt": "2024-04-08T19:32:31.466Z",
-              "updatedAt": "2024-04-08T19:32:31.466Z",
-              "category": null
-            },
-            {
-              "id": "add3b160-b1d4-4e8e-b60f-7bb9745411ac",
-              "value": 2000,
-              "description": "Sobre alguma coisa",
-              "type": "INCOME",
-              "userId": "6a04f4a5-4df3-4b1b-b4ea-02efbe5ea9ce",
-              "categoryId": null,
-              "createdAt": "2024-04-08T19:32:39.975Z",
-              "updatedAt": "2024-04-08T19:32:39.975Z",
-              "category": null
-            },
-            {
-              "id": "195a92dc-8fa7-41ac-9d1f-48cd1c545250",
-              "value": 1000,
-              "description": "Novo Ganho",
-              "type": "INCOME",
-              "userId": "6a04f4a5-4df3-4b1b-b4ea-02efbe5ea9ce",
-              "categoryId": null,
-              "createdAt": "2024-04-08T19:37:00.037Z",
-              "updatedAt": "2024-04-08T19:37:00.037Z",
-              "category": null
-            }];
-
-        console.log(transactions);
+        const transactions = data.body;
 
         const incomes = transactions.filter(({ type }) => type === "INCOME");
         const expenses = transactions.filter(({ type }) => type === "EXPENSE");
@@ -298,7 +263,7 @@ formIncome.addEventListener("submit", async (event) => {
     })
     .then((data) => {
       formIncome.reset();
-      getData();
+      getUserData();
     })
     .catch((error) => {
       console.error("this", error);
@@ -337,7 +302,7 @@ formExpense.addEventListener("submit", async (event) => {
     })
     .then((data) => {
       formExpense.reset();
-      getData();
+      getUserData();
       getBiggestExpenseMonth();
     })
     .catch((error) => {
